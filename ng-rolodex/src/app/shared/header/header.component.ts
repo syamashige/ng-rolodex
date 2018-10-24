193276d26f535b77f1ba6033b55beb0a37c616a8
Sarah <yamashige.sarah@gmail.com>
2018-10-23 21:10:31 -1000

    Initial Commit

---
+++ ng-rolodex/src/app/shared/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
+})
+
+export class HeaderComponent {
+
+    title: string = 'header'
+
+    constructor() {
+        
+    }
+}
